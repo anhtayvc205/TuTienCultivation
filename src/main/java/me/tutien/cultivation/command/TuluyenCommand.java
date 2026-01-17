775d9c6331724e055ece5f48f18a4cfde4c039e4
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-17 19:03:22 +0700

    Create TuluyenCommand.java

---
+++ src/main/java/me/tutien/cultivation/command/TuluyenCommand.java
@@ -0,0 +1,23 @@
+package me.tutien.cultivation.command;
+
+import me.tutien.cultivation.TuTienCultivation;
+import me.tutien.cultivation.data.PlayerCultivationData;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+public class TuluyenCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
+        if (!(s instanceof Player p)) return true;
+
+        PlayerCultivationData data =
+                TuTienCultivation.storage().get(p.getUniqueId());
+
+        data.setCultivating(!data.isCultivating());
+        p.sendMessage(data.isCultivating()
+                ? "§aBắt đầu tu luyện"
+                : "§cDừng tu luyện");
+        return true;
+    }
+}
