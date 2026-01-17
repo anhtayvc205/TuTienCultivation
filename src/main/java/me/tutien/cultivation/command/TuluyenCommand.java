519209af9ac6629173f0482571c87cc426990097
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-17 21:30:19 +0700

    Update TuluyenCommand.java

--- src/main/java/me/tutien/cultivation/command/TuluyenCommand.java
+++ src/main/java/me/tutien/cultivation/command/TuluyenCommand.java
@@ -1,25 +1,21 @@
 package me.tutien.cultivation.command;
 
 import me.tutien.cultivation.TuTienCultivation;
-import me.tutien.cultivation.dao.DaoType;
-import me.tutien.cultivation.data.PlayerCultivationData;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
-public class TutienCommand implements CommandExecutor {
+public class TuluyenCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
         if (!(s instanceof Player p)) return true;
 
-        PlayerCultivationData d = TuTienCultivation.storage().get(p.getUniqueId());
-        if (d.getDao() != DaoType.NONE) {
-            p.sendMessage("§cBạn đã tu luyện rồi!");
-            return true;
-        }
+        var d = TuTienCultivation.storage().get(p.getUniqueId());
+        d.setCultivating(!d.isCultivating());
 
-        d.chooseDao(DaoType.CHINH_DAO);
-        p.sendMessage("§aBạn đã bước vào con đường tu tiên!");
+        p.sendMessage(d.isCultivating()
+                ? "§aBắt đầu tu luyện..."
+                : "§cDừng tu luyện");
         return true;
     }
 }
