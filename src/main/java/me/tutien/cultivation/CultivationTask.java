f2cc0266d51a016a09dbcc69eea4c4f750082d13
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-17 20:44:15 +0700

    Create CultivationTask.java

---
+++ src/main/java/me/tutien/cultivation/CultivationTask.java
@@ -0,0 +1,24 @@
+package me.tutien.cultivation;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class CultivationTask extends BukkitRunnable {
+
+    @Override
+    public void run() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            PlayerCultivationData data =
+                    TuTienCultivation.storage().get(p.getUniqueId());
+
+            if (data.isCultivating()) {
+                data.addKhi();
+                p.sendActionBar(
+                        "§e" + data.getStage().name() +
+                        " §7(" + data.getLinhKhi() + "/" + data.getStage().maxKhi + ")"
+                );
+            }
+        }
+    }
+}
