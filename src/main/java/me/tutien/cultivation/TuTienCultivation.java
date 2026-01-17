10f1594269f368ac280cc873768506797828c034
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-17 21:26:19 +0700

    Update TuTienCultivation.java

--- src/main/java/me/tutien/cultivation/TuTienCultivation.java
+++ src/main/java/me/tutien/cultivation/TuTienCultivation.java
@@ -1,5 +1,8 @@
 package me.tutien.cultivation;
 
+import me.tutien.cultivation.command.*;
+import me.tutien.cultivation.listener.CultivationListener;
+import me.tutien.cultivation.storage.CultivationStorage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TuTienCultivation extends JavaPlugin {
@@ -10,14 +13,15 @@ public class TuTienCultivation extends JavaPlugin {
     public void onEnable() {
         storage = new CultivationStorage();
 
+        getCommand("tutien").setExecutor(new TutienCommand());
         getCommand("tuluyen").setExecutor(new TuluyenCommand());
         getCommand("dotpha").setExecutor(new DotPhaCommand());
 
-        getServer().getPluginManager().registerEvents(new CombatListener(), this);
+        getServer().getPluginManager().registerEvents(
+                new CultivationListener(), this
+        );
 
-        new CultivationTask().runTaskTimer(this, 20, 20);
-
-        getLogger().info("TuTienCultivation ENABLED (Part 1)");
+        getLogger().info("TuTienCultivation PHẦN 1 loaded");
     }
 
     public static CultivationStorage storage() {
