a7ce014df526d28882d63164cf137ab76ad70ec3
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-17 23:46:23 +0700

    Update DotPhaCommand.java

--- src/main/java/me/tutien/cultivation/command/DotPhaCommand.java
+++ src/main/java/me/tutien/cultivation/command/DotPhaCommand.java
@@ -1,18 +1,16 @@
 package me.tutien.cultivation.command;
 
-import me.tutien.cultivation.TuTienCultivation;
+import me.tutien.cultivation.*;
 import me.tutien.cultivation.thienkiep.ThienKiep;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
 public class DotPhaCommand implements CommandExecutor {
 
-    @Override
     public boolean onCommand(CommandSender s, Command c, String l, String[] a) {
         if (!(s instanceof Player p)) return true;
 
         var d = TuTienCultivation.storage().get(p.getUniqueId());
-
         if (!d.canDotPha()) {
             p.sendMessage("§cChưa đủ linh khí!");
             return true;
