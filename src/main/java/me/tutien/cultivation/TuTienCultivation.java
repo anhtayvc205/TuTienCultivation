8ed7cc0ce2ecef8e429fbcacf5068c3836fbbf5c
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-17 21:52:18 +0700

    Update TuTienCultivation.java

--- src/main/java/me/tutien/cultivation/TuTienCultivation.java
+++ src/main/java/me/tutien/cultivation/TuTienCultivation.java
@@ -13,15 +13,13 @@ public class TuTienCultivation extends JavaPlugin {
     public void onEnable() {
         storage = new CultivationStorage();
 
-        getCommand("tutien").setExecutor(new TutienCommand());
-        getCommand("tuluyen").setExecutor(new TuluyenCommand());
+        getCommand("tutien").setExecutor(new TuTienCommand());
+        getCommand("tuluyen").setExecutor(new TuLuyenCommand());
         getCommand("dotpha").setExecutor(new DotPhaCommand());
 
         getServer().getPluginManager().registerEvents(
                 new CultivationListener(), this
         );
-
-        getLogger().info("TuTienCultivation PHẦN 1 loaded");
     }
 
     public static CultivationStorage storage() {
