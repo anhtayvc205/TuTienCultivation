dc2a4cf2ea4063c087dc2e5a3d2c854ffa8c1869
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 08:42:23 +0700

    Update CultivationTask.java

--- src/main/java/me/tutien/cultivation/task/CultivationTask.java
+++ src/main/java/me/tutien/cultivation/task/CultivationTask.java
@@ -1,5 +1,6 @@
 package me.tutien.cultivation;
 
+import me.tutien.cultivation.data.PlayerCultivationData;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
