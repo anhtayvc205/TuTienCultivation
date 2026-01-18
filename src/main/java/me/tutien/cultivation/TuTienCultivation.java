bd682aebd377aae425d231b37440fa917a75ed28
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 08:45:59 +0700

    Update TuTienCultivation.java

--- src/main/java/me/tutien/cultivation/TuTienCultivation.java
+++ src/main/java/me/tutien/cultivation/TuTienCultivation.java
@@ -1,19 +1,45 @@
 package me.tutien.cultivation;
 
 import me.tutien.cultivation.command.*;
+import me.tutien.cultivation.listener.CombatListener;
+import me.tutien.cultivation.listener.CultivationListener;
 import me.tutien.cultivation.storage.CultivationStorage;
+import me.tutien.cultivation.task.CultivationTask;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TuTienCultivation extends JavaPlugin {
+public final class TuTienCultivation extends JavaPlugin {
 
+    private static TuTienCultivation instance;
     private static CultivationStorage storage;
 
     @Override
     public void onEnable() {
+        instance = this;
         storage = new CultivationStorage();
 
+        // ===== COMMAND =====
+        getCommand("tutien").setExecutor(new TutienCommand());
+        getCommand("tuluyen").setExecutor(new TuluyenCommand());
         getCommand("dotpha").setExecutor(new DotPhaCommand());
-        getCommand("tuluyen").setExecutor(new TuLuyenCommand());
+
+        // ===== LISTENER =====
+        getServer().getPluginManager().registerEvents(new CombatListener(), this);
+        getServer().getPluginManager().registerEvents(new CultivationListener(), this);
+
+        // ===== TASK =====
+        new CultivationTask().runTaskTimer(this, 20, 20);
+
+        getLogger().info("§aTuTienCultivation ENABLED (Paper 1.21.8)");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("§cTuTienCultivation DISABLED");
+    }
+
+    // ===== STATIC ACCESS =====
+    public static TuTienCultivation getInstance() {
+        return instance;
     }
 
     public static CultivationStorage storage() {
