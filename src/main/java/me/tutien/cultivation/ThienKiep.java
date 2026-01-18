582fa252802e261b9c22fee58bff3ae61f301ae1
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 09:10:20 +0700

    Create ThienKiep.java

---
+++ src/main/java/me/tutien/cultivation/ThienKiep.java
@@ -0,0 +1,11 @@
+package me.tutien.cultivation;
+
+import org.bukkit.entity.Player;
+
+public class ThienKiep {
+
+    public static void start(Player p, PlayerCultivationData data) {
+        p.sendMessage("§c⚡ Thiên kiếp đang hình thành trên bầu trời...");
+        // Message 3 sẽ gắn boss + wave + skill
+    }
+}
