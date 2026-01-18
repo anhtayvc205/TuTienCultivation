d80d2807935c924cd9ba249b1bdfbae271a46466
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 09:24:55 +0700

    Update CultivationStorage.java

--- src/main/java/me/tutien/cultivation/CultivationStorage.java
+++ src/main/java/me/tutien/cultivation/CultivationStorage.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class CultivationStorage {
 
-    private final Map<UUID, PlayerCultivationData> map = new HashMap<>();
+    private final Map<UUID, PlayerCultivationData> data = new HashMap<>();
 
     public PlayerCultivationData get(UUID uuid) {
-        return map.computeIfAbsent(uuid, k -> new PlayerCultivationData());
+        return data.computeIfAbsent(uuid, PlayerCultivationData::new);
     }
 }
